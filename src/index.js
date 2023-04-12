dbb14a3056df5b3eacd2738df7486de8a8625723
Kurt Hakim <kurthakim@gmail.com>
2023-04-13 00:24:15 +0700

    Displaying an individual movie

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import EditMovie from './components/EditMovie';
 import ManageCatalogue from './components/ManageCatalogue';
 import GraphQL from './components/GraphQL';
 import Login from './components/Login';
+import Movie from './components/Movie';
 
 
 const router = createBrowserRouter([
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
       {
         path: "/movies",
         element: <Movies />,
+      },
+      {
+        path: "/movies/:id",
+        element: <Movie />,
       },
       {
         path: "/genres",
